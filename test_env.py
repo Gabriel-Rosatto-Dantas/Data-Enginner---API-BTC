3c98fc2fc727c2f53274ab02a1811c78c78bf5b9
Gabriel-Rosatto-Dantas <pessoalgabrielrosatto@gmail.com>
2025-05-26 15:35:43 -0300

    excluir teste

--- test_env.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-database_url = os.getenv("DATABASE_KEY")
-print("String de conexão encontrada:", database_url is not None)
-if database_url:
-    print("Primeiros 20 caracteres da string:", database_url[:20] + "...") 
